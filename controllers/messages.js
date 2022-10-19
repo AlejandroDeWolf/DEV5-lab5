8591954b952761907eef0a9af29ade27de743b83
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-10-19 17:52:21 +0200

    feat: create function to return message with id from url

--- controllers/messages.js
+++ controllers/messages.js
@@ -7,9 +7,20 @@ const getAll = (req, res) => {
         status: 'success',
         message: 'GETTING messages',
         data: messages
-
     }
     res.json(response);
 };
 
+const getById = (req, res) => {
+    const id = req.params.id;
+    const response = {
+        status: 'success',
+        message: `GETTING message with id ${id}`,
+        data: { id: id, user: "John", message: 'Hello' }
+    }
+    res.json(response);
+};
+
+
 module.exports.getAll = getAll;
+module.exports.getById = getById;
